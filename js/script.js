aa805c2a254f246440388b91466f132115e6c57e
Yaroslav <nogin.yarik@gmail.com>
2021-04-08 10:21:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+/* 
+const obj = {
+    name: 'Yaroslav',
+    age: 26,
+    isMarried: true
+};
+
+console.log(obj.name);
+// console.log(obj['name']);
+
+let array = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []]; 
+console.log(array[1]);
+*/
+
+
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');  // используем унарный плюс "+"
+// console.log(answer + 5);
+
+
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как вашa фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+
+
+// 008 ИНТЕРПОЛЯЦИЯ
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Yaroslav';
+// alert(`Привет, ${user}`);
+
+
+
+// 009 ОПЕРАТОРЫ В JS
+// console.log(4 + +'5');
+
+// let incr = 10;
+//     decr = 10;
+
+// incr++;   // постфиксная форма
+// decr--;
+// // ++incr;   // префиксная форма
+// // --decr;
+// console.log(incr);
+// console.log(decr);
+
+// console.log(5%2);   // остаток 1
+
+// console.log(2*4 == 8);   // "==" оператор сравнения, true или false.
+// console.log(2*4 === '8');   // "===" оператор строгого сравнения.
+// console.log(2 + 2 * 4 != 8);   // "!=" не равенство
+// console.log(2 + 2 * 2 != '6');   // false
+// console.log(2 + 2 * 2 !== '6');   // true
+
+// const isChacked = true,
+//       isClose = false;
+// console.log(isChacked && isClose);
+// console.log(isChacked || isClose);
+// console.log(isChacked && !isClose);
+
+
+
